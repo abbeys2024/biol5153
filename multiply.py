803c230b333c9706ed5c9f82e087b45a39872221
Abbey Bryan <abbeyb@uark.edu>
2024-04-01 09:19:09 -0500

    Divided code into functions

--- multiply.py
+++ multiply.py
@@ -1,12 +1,25 @@
 #! /usr/bin/env python3
-user_num = int(input('Enter the size of your table:'))
-user_num += 1
 
-cell_width = len(str((user_num * user_num))) + 1
-for k in range(1,user_num):
-    for j in range(1,user_num):
-        print('{:>{cell_width}}'.format(k*j,cell_width = cell_width), end = '')
-    print()
+def get_input():
+    user_num = int(input('Enter the size of your table:'))
+    user_num += 1
+    return user_num
+
+def print_table(user_num):
+    cell_width = len(str((user_num * user_num))) + 1
+    for k in range(1,user_num):
+        for j in range(1,user_num):
+            print('{:>{cell_width}}'.format(k*j,cell_width = cell_width), end = '')
+        print()
+
+def main():
+    input_num = get_input()
+    print_table(input_num)
+
+# set the environment for this script
+# is it main, or is this a module being called by another script
+if __name__ == '__main__':
+    main()
 
 
 
