129bc8bb2ae5296f40d5bc46c8ad2b1ce66b5acf
Abbey Bryan <abbeyb@uark.edu>
2024-04-01 09:04:45 -0500

    Added formatting so allow space no matter the size of table

--- multiply.py
+++ multiply.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python3
 user_num = int(input('Enter the size of your table:'))
 user_num += 1
+
+cell_width = len(str((user_num * user_num))) + 1
 for k in range(1,user_num):
     for j in range(1,user_num):
-        print('{:^4}'.format(k*j), end = ' ')
+        print('{:>{cell_width}}'.format(k*j,cell_width = cell_width), end = '')
     print()
 
 
